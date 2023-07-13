ef1951be407fa11cd51885f0b258dec36b438f72
sanidhya99 <100680783+sanidhya99@users.noreply.github.com>
2023-07-13 14:56:48 +0530

    Update urls.py

--- dealing/urls.py
+++ dealing/urls.py
@@ -2,10 +2,14 @@ from .views import *
 from django.urls import path
 
 urlpatterns=[
+    #url to create and list products
     path("products/" ,productpost.as_view(),name="Create-List-products"),
+    #url to change or delete products
     path("products/<str:name>/" ,productchange.as_view(),name="Modify-products"),
+    #url to create and list orders
     path("delivery/" ,DeliveryListCreateView.as_view(),name="Create-deliveries"),
-    # path("delivery/delete/<int:reciever_id>/" ,DeliveryUD.as_view(),name="Modify-deliveries"),
+     #url to delete orders
+    path("delivery/delete/<int:reciever_id>/" ,DeliveryUD.as_view(),name="Modify-deliveries"),
     
 
            ]
