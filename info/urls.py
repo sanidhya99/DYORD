befa80ea5ad3b0c63e1e0d9fb36df440b298a701
sanidhya99 <100680783+sanidhya99@users.noreply.github.com>
2023-07-13 14:55:17 +0530

    Update urls.py

--- info/urls.py
+++ info/urls.py
@@ -2,7 +2,9 @@ from .views import *
 from django.urls import path
 
 urlpatterns=[
+    #url to create and list blog 
     path("blogs/" ,blogpost.as_view(),name="Create-List-blogs"),
+    #url to change or delete blog
     path("blogs/<str:topic>/" ,blogchange.as_view(),name="Modify-blogs"),
     
 
